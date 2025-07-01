ee4dd3ae4c473ff5e075e1fce316c4c94d75585e
Adilet Nisharapov <nisharapov.adilet@gmail.com>
2025-07-01 17:09:20 +0200

    update 01.06 17h09

--- app/src/Controller/BookController.php
+++ app/src/Controller/BookController.php
@@ -9,13 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
-
     #[Route('/book/{id}', name: 'app_book', requirements: ['id' => '\d+'])]
     public function book(Offerings $offering): Response
     {
+        // Mapper les propriétés de l'entité Offerings dans un tableau
+        $offeringData = [
+            'id' => $offering->getId(),
+            'price' => $offering->getPrice() ?? 0.0, // Valeur par défaut si null
+            'name' => $offering->getName() ?? 'Offre inconnue', // Valeur par défaut si null
+        ];
 
         return $this->render('book/index.html.twig', [
-            'offering' => $offering, // Passer les données mappées
+            'offering' => $offeringData, // Passer le tableau mappé
         ]);
     }
 }
